b60fa20252abb4b4c91cb51f26d52c896e76ee38
joshjms <83194022+joshjms@users.noreply.github.com>
2024-02-29 17:59:33 +0800

    add public field

--- filesystem/directory.go
+++ filesystem/directory.go
@@ -5,4 +5,5 @@ type Directory struct {
 	Name     string // Directory name
 	Owner    int    // Owner of the directory, this references a user ID
 	ParentID int    // Optional parent directory ID, allows for root directories
+	Public   bool   // Whether the directory is public or not
 }
