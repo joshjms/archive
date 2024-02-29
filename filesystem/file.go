2cbc3eff59c4a12807b86dffd20823a6fdab060f
joshjms <83194022+joshjms@users.noreply.github.com>
2024-02-29 18:21:14 +0800

    modify fields

--- filesystem/file.go
+++ filesystem/file.go
@@ -1,13 +1,10 @@
 package fs
 
 type File struct {
-	ID       int    `gorm:"primaryKey"`       // File ID
-	Name     string `gorm:"unique; not null"` // Unique and non-null file name
+	ID       int    `gorm:"primaryKey"` // File ID
+	Name     string `gorm:"not null"`   // Unique and non-null file name
 	ParentID int    // ID of the parent directory
 	Dir      string
 	Public   bool
-}
-
-func (f *File) Read() {
-
+	Slug     string `gorm:"unique"` // Unique slug for the file
 }
