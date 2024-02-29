010a872b19312e693dac69ab2c4b08ea44dbfe4e
joshjms <83194022+joshjms@users.noreply.github.com>
2024-02-29 16:25:56 +0800

    create endpoint for directory testing

--- api/api.go
+++ api/api.go
@@ -3,17 +3,23 @@ package api
 import (
 	"net/http"
 
-	"github.com/joshjms/archive/filesystem"
+	fs "github.com/joshjms/archive/filesystem"
 	"github.com/labstack/echo/v4"
 )
 
-func Init(fs *filesystem.FileSystem) {
-
+func Init(fs *fs.FileSystem) {
 	e := echo.New()
 
 	// Routes
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/files/*", func(c echo.Context) error {
+		// Get the path from the URL
+		path := c.Param("*")
+
+		return c.String(http.StatusOK, path)
 	})
 
 	// Start server
