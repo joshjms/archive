b350eabbf068c3560d07ac328354358e7cd034bd
joshjms <83194022+joshjms@users.noreply.github.com>
2024-02-29 18:16:02 +0800

    remove owner field

--- filesystem/directory.go
+++ filesystem/directory.go
@@ -3,7 +3,6 @@ package fs
 type Directory struct {
 	ID       int    `gorm:"primaryKey"`
 	Name     string // Directory name
-	Owner    int    // Owner of the directory, this references a user ID
 	ParentID int    // Optional parent directory ID, allows for root directories
 	Public   bool   // Whether the directory is public or not
 	Slug     string // Unique slug for the directory
