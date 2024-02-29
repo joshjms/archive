6b1b94e7fd8afae86f4ac20a75304f1d1f880026
joshjms <83194022+joshjms@users.noreply.github.com>
2024-02-29 18:29:12 +0800

    add database functions

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -20,3 +20,19 @@ func (fs *FileSystem) Init() {
 	fs.DB.AutoMigrate(&FilePermission{})
 	fs.DB.AutoMigrate(&DirectoryPermission{})
 }
+
+func (fs *FileSystem) CreateFile(file *File) error {
+	return fs.DB.Create(file).Error
+}
+
+func (fs *FileSystem) GetFile(id int) (*File, error) {
+	file := &File{}
+	err := fs.DB.First(file, id).Error
+	return file, err
+}
+
+func (fs *FileSystem) GetFileFromSlug(slug string) (*File, error) {
+	file := &File{}
+	err := fs.DB.Where("name = ?", slug).First(file).Error
+	return file, err
+}
